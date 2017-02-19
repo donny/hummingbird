1b4d0139fccd2bea67bd59f03d670dcd538f23b4
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-02-20 08:10:33 +1100

    Add initial Xcode files

---
+++ Hummingbird/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hummingbird
+//
+//  Created by Donny Kurniawan on 20/2/17.
+//  Copyright © 2017 Donny Kurniawan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
